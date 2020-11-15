caa3f4e4527e38c03d29752d8e7dbda4c6b08898
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-11-15 18:24:10 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,12 @@ end
 def runner
   welcome 
   card_total = initial_round
-  until card_total > 21
-    card_total = hit?
+  
+  until card_total > 21 do
+    card_total = hit?(card_total)
+  end
+  
+  end_game(card_total)
   
 end
     
